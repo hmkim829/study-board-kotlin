1098a467c93eb43053bb79f7db92f1687515e208
hmkim829 <hmkim829@gmail.com>
2024-06-16 17:25:19 +0900

    feat: project initial setting
    mvc, jpa, h2, mysql, swagger, querydsl, ktlint, kotest

---
+++ src/main/kotlin/com/study/board/StudyBoardKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.study.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyBoardKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyBoardKotlinApplication>(*args)
+}
